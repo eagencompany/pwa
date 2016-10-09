43a840edbf479e40594bc2e387f3b68c1e5c16a2
eagencompany <eagencompany@gmail.com>
2016-10-09 18:18:13 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var dataCacheName = 'weatherData-v2';
+var dataCacheName = 'weatherData-v3';
 var cacheName = 'weatherPWA-final-1';
 var filesToCache = [
   '/pwa',
